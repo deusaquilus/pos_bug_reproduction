e0240f56cf08885564d40dc51b2430a13676e888
Alexander Ioffe <alexander.ioffe@nasdaq.com>
2020-12-13 08:21:11 -0500

    Simplify

--- src/main/scala/io/getquill/ContextRet.scala
+++ src/main/scala/io/getquill/ContextRet.scala
@@ -3,10 +3,8 @@ package io.getquill
 import scala.quoted._
 
 object ContextRet {
-
-  def runTestImpl(quoted: Expr[MyQuoted])(using qctx: Quotes): Expr[String] = {
+  def runTestImpl(quoted: Expr[MyQuoted])(using qctx: Quotes): Expr[String] =
     '{ $quoted.ast.toString }
-  }
 
   inline def runAndTest(inline quoted: MyQuoted): String = 
     ${ runTestImpl('quoted) }
--- src/main/scala/io/getquill/MyQuoteMacro.scala
+++ src/main/scala/io/getquill/MyQuoteMacro.scala
@@ -8,6 +8,7 @@ object MyQuoteMacro {
   inline def myquote: MyQuoted = ${ MyQuoteMacro.apply }
   def apply(using Quotes): Expr[MyQuoted] = {
     import quotes.reflect._
+
     '{ MyQuoted("p", ${Expr.ofList(List( '{ "foo" } ))}) }
   }
 }
@@ -15,10 +16,8 @@ object MyQuoteMacro {
 // If you put this into here, then it works!
 /*
 object ContextRet {
-
-  def runTestImpl(quoted: Expr[MyQuoted])(using qctx: Quotes): Expr[String] = {
+  def runTestImpl(quoted: Expr[MyQuoted])(using qctx: Quotes): Expr[String] =
     '{ $quoted.ast.toString }
-  }
 
   inline def runAndTest(inline quoted: MyQuoted): String = 
     ${ runTestImpl('quoted) }
