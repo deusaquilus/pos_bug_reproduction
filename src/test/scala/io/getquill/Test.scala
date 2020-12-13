00e79b8b36b1fae495b803e026c36ca996fc6dd0
Alexander Ioffe <alexander.ioffe@nasdaq.com>
2020-12-13 08:46:01 -0500

    Uneeded comment

--- src/test/scala/io/getquill/Test.scala
+++ src/test/scala/io/getquill/Test.scala
@@ -5,6 +5,6 @@ object Test {
   inline def q2 = MyQuoteMacro.myquote
 
   def main(args: Array[String]): Unit = {
-    println( PullAst.apply( q2 ) ) //helloooooooo
+    println( PullAst.apply( q2 ) )
   }
 }
